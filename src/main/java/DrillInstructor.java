9f670ac652ad4da1aed44517259f4fc590584014
vvmk <vincentmasiello@gmail.com>
2018-02-21 00:22:01 -0500

    sweep under rug

--- src/main/java/DrillInstructor.java
+++ src/main/java/DrillInstructor.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.Arrays;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 
@@ -9,11 +10,15 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * date: 2/12/18
  */
 public class DrillInstructor {
+    /** Swing tests */
+    private static JLabel titleLabel;
+    private static JLabel tickLabel;
+    private static JButton startButton;
 
     private static final int period = 1000;             // tick every second
     private static final int DEFAULT_BREAK_TIME = 5;    // default 5 seconds between drills
     private int breakTime;
-    private int secondsLeft;
+    private static int secondsLeft;
     private ConcurrentLinkedQueue<Drill> drills;
 
     public DrillInstructor() {
@@ -31,9 +36,47 @@ public class DrillInstructor {
 
     public void runDrills() {
         secondsLeft = 5;
-        Timer startCountdown = new Timer(1000, e -> {
-            System.out.println(5 - (e.getWhen() - System.currentTimeMillis()));
+        Timer countdown = new Timer(period, e -> {
+            System.out.println("ticking");
         });
-        startCountdown.start();
+    }
+
+    public static void setupGUI() {
+        JFrame frame = new JFrame("SSR"); //create jframe
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //define close method
+
+        /** setup components */
+        //tickLabel = new JLabel("");
+        titleLabel = new JLabel("SHFFL Shine Repeat");
+        //startButton = new JButton("Start");
+
+        secondsLeft = 5;
+
+//        /** tests */
+//        Drill d = new Drill("Drill #1 Title", 5000);
+//        Timer t = new Timer(d.getTime(), e -> {
+//            if (secondsLeft > 0) {
+//                titleLabel.setText(d.getTitle());
+//                tickLabel.setText(String.format("%d", secondsLeft));
+//            }
+//            System.out.println(--secondsLeft);
+//        });
+//
+//        /** set up start button action, probably */
+//        startButton.addActionListener(e -> {
+//            t.start();
+//            System.out.println("button click");
+//        });
+
+        //frame.getContentPane().add(startButton);
+        frame.getContentPane().add(titleLabel);
+        //frame.getContentPane().add(tickLabel);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(DrillInstructor::setupGUI);
     }
 }
--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 /**
@@ -8,28 +9,42 @@ import java.util.Scanner;
  */
 
 public class Driver {
+    public static void createAndShowGUI() {
+        JFrame frame = new JFrame("SHFFL Shine Repeat");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("create a queue of drills for more efficient practicing");
+        frame.getContentPane().add(label);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        DrillInstructor drillInstructor = new DrillInstructor();
-
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("How many drills?");
-        Integer numberOfTimers = Integer.parseInt(in.nextLine());
-
-        System.out.println("How much time before each drill (in seconds)?");
-        Integer breakTime = Integer.parseInt(in.nextLine());
-
-        // build the queue
-        for (int i=0;i<numberOfTimers;i++) {
-            System.out.println("Drill #" + (i+1));
-
-            System.out.print("Title: ");
-            String title = in.nextLine();
-
-            System.out.print("Seconds: ");
-            Integer timeSeconds = Integer.parseInt(in.nextLine()); //TODO: enforce a limit or change to Double
-
-            drillInstructor.enqueue(new Drill(title, timeSeconds));
-        }
+        javax.swing.SwingUtilities.invokeLater(() -> {
+                createAndShowGUI();
+        });
+//       /* *//*DrillInstructor drillInstructor = new DrillInstructor();
+//
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.println("How many drills?");
+//        Integer numberOfTimers = Integer.parseInt(in.nextLine());
+//
+//        System.out.println("How much time before each drill (in seconds)?");
+//        Integer breakTime = Integer.parseInt(in.nextLine());
+//
+//        // build the queue
+//        for (int i=0;i<numberOfTimers;i++) {
+//            System.out.println("Drill #" + (i+1));
+//
+//            System.out.print("Title: ");
+//            String title = in.nextLine();
+//
+//            System.out.print("Seconds: ");
+//            Integer timeSeconds = Integer.parseInt(in.nextLine()); //TODO: enforce a limit or change to Double
+//
+//            drillInstruct*//*or.enqueue(new Drill(title, timeSeconds));*/
+//        }
     }
 }
